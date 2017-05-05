678ab31c59cc38303c772d1b28559b0caa0cf670
KisleK <kisle.kuhn1@gmail.com>
2017-05-04 19:32:34 -0500

    added form params

--- app/controllers/params_games_controller.rb
+++ app/controllers/params_games_controller.rb
@@ -11,8 +11,8 @@ class ParamsGamesController < ApplicationController
 
  
     def guess
-    @user_guess = params["submission"].to_i
-    winning_number = 36
+     @user_guess = params["submission"].to_i
+     winning_number = 42
 
       if winning_number < @user_guess
         @answer_message = "too high"
@@ -27,7 +27,7 @@ class ParamsGamesController < ApplicationController
 
 
    def segement_guess
-       @user_guess = params["submission"].to_i
+       @user_guess = params["wildcard"].to_i
        winning_number = 36
 
          if winning_number < @user_guess
@@ -41,4 +41,31 @@ class ParamsGamesController < ApplicationController
         end
       end
 
+      def form_show
+
+      end 
+      def form_send
+        @message = params["form_message"]
+
+      end
+
+      def number_show
+      end
+
+      def number_send
+        @user_guess = params["guess"].to_i
+        winning_number = 42
+          if winning_number < @user_guess
+            @guess_message = "too high"
+
+          elsif winning_number > @user_guess
+            @guess_message = "to low"
+
+          else
+            @guess__message = "Bingo- bango"
+          end
+      end
+      
+
+     
    end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 get '/params_game' => 'params_games#query'
 get '/guess' => 'params_games#guess'
 
-get '/segement_guess/:wildcard' => 'params_games#segement_guess'
+get '/segement_guess/:submission' => 'params_games#segement_guess'
+
+get '/form_show_params' => 'params_games#form_show'
+post '/form_send_params' => 'params_games#form_send'
+
+get '/number_show_params' => 'params_games#number_show'
+post '/number_send_params' => 'params_games#number_send'
 end
